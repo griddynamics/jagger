c4bb2c95b2f99b5f9942ec2281f83525cf054d56
anton-antonenko <kingtoxa@gmail.com>
2016-09-28 18:40:43 +0300

    JFG-890: Add basic implementation to JSpringBasedHttpClient

--- chassis/invokers/src/main/java/com/griddynamics/jagger/invoker/http/v2/JHttpEndpoint.java
+++ chassis/invokers/src/main/java/com/griddynamics/jagger/invoker/http/v2/JHttpEndpoint.java
@@ -1,6 +1,10 @@
 package com.griddynamics.jagger.invoker.http.v2;
 
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
+
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 
 /**
@@ -17,7 +21,13 @@ public class JHttpEndpoint {
      * Enum representing HTTP and HTTPS protocols
      */
     public enum Protocol {
-        HTTP, HTTPS
+        HTTP("http"), HTTPS("https");
+
+        private String value;
+
+        Protocol(String value) {
+            this.value = value;
+        }
     }
 
     private Protocol protocol = Protocol.HTTP;
@@ -53,7 +63,7 @@ public class JHttpEndpoint {
      * {@link JHttpEndpoint#hostname} and {@link JHttpEndpoint#port} fields values.
      */
     public URI getURI() {
-        throw new UnsupportedOperationException();
+        return URI.create(String.format("%s://%s:%s", protocol.value, hostname, port));
     }
 
     /**
@@ -62,6 +72,34 @@ public class JHttpEndpoint {
      * {@link JHttpEndpoint#hostname} and {@link JHttpEndpoint#port} fields values with <b>queryParams</b> added.
      */
     public URI getURI(Map<String, String> queryParams) {
-        throw new UnsupportedOperationException();
+        URI uri = getURI();
+        if (MapUtils.isEmpty(queryParams))
+            return uri;
+
+        return appendParameters(uri, queryParams);
+    }
+
+    /**
+     * @param oldUri base {@link URI}
+     * @param queryParams query parameters to be added to {@link URI}
+     * @return {@link URI} based on oldUri with <b>queryParams</b> added.
+     */
+    public static URI appendParameters(URI oldUri, Map<String, String> queryParams) {
+        String newQuery = oldUri.getQuery();
+        String appendQuery = queryParams.entrySet().stream()
+                .map(param -> param.getKey() + "=" + param.getValue())
+                .reduce("", (a, b) -> a + "&" + b);
+
+        if (newQuery == null) {
+            newQuery = StringUtils.removeStart(appendQuery, "&");
+        } else {
+            newQuery += appendQuery;
+        }
+
+        try {
+            return new URI(oldUri.getScheme(), oldUri.getAuthority(), oldUri.getPath(), newQuery, oldUri.getFragment());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- chassis/invokers/src/main/java/com/griddynamics/jagger/invoker/http/v2/JSpringBasedHttpClient.java
+++ chassis/invokers/src/main/java/com/griddynamics/jagger/invoker/http/v2/JSpringBasedHttpClient.java
@@ -1,16 +1,52 @@
 package com.griddynamics.jagger.invoker.http.v2;
 
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 import java.util.Map;
 
 /**
- * Created by aantonenko on 9/27/16.
+ * Implementation of {@link JHttpClient}. <p>
+ * This implementation is based on the Spring {@link RestTemplate}.
+ *
+ * @author Anton Antonenko
+ * @see JHttpClient
+ * @since 1.2.7
  */
+@SuppressWarnings({"unused", "unchecked"})
 public class JSpringBasedHttpClient implements JHttpClient {
 
     private Map<String, Object> clientParams;
 
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public JSpringBasedHttpClient(Map<String, Object> clientParams) {
+        this.clientParams = clientParams;
+    }
+
     @Override
     public JHttpResponse execute(JHttpEndpoint endpoint, JHttpQuery query) {
-        throw new UnsupportedOperationException();
+        clientParams.putAll(query.getClientParams());
+
+        URI endpointURI = endpoint.getURI(query.getQueryParams());
+
+        RequestEntity requestEntity = mapToRequestEntity(query, endpointURI);
+        ResponseEntity responseEntity = restTemplate.exchange(endpointURI, query.getMethod(), requestEntity, Object.class);
+
+        return mapToJHttpResponse(responseEntity);
+    }
+
+    private <T> RequestEntity<T> mapToRequestEntity(JHttpQuery<T> query, URI endpointURI) {
+        return new RequestEntity<>(query.getBody(), query.getHeaders(), query.getMethod(), endpointURI);
+    }
+
+    private <T> JHttpResponse<T> mapToJHttpResponse(ResponseEntity<T> responseEntity) {
+        JHttpResponse<T> jHttpResponse = new JHttpResponse<>();
+        jHttpResponse.setHeaders(responseEntity.getHeaders());
+        jHttpResponse.setBody(responseEntity.getBody());
+        jHttpResponse.setStatus(responseEntity.getStatusCode());
+        return jHttpResponse;
     }
 }
