a523e3869075838cad3847e98e1891e4a6020d8e
Andrey Badaev <abadaev@griddynamics.com>
2016-10-05 13:08:52 +0300

    [JFG-861] More detailed logs.

--- docker/jagger/webclient/start.sh
+++ docker/jagger/webclient/start.sh
@@ -2,7 +2,7 @@
 
 # Docker variables
 IMAGE_NAME="jagger-web-client"
-VERSION_TAG="1.2.6"
+VERSION_TAG="1.3"
 CONTAINER_NAME="jwc"
 HTTP_HOST_PORT=8080
 
@@ -13,10 +13,8 @@ JWC_JDBC_URL=jdbc:mysql://172.18.128.228:3306/jaggerdb
 JWC_JDBC_USER=root
 JWC_JDBC_PASS=root
 
-echo "In start script for ${IMAGE_NAME}:${VERSION_TAG} Docker image..."
-
 # Step 1. Check and build the image if needed.
-echo "'Check and build the image if needed' phase..."
+echo "'Check and build the image if needed' phase for ${IMAGE_NAME} image..."
 is_image_created=$(docker images -q ${IMAGE_NAME})
 if [ -z "${is_image_created}" ]; then
     echo "Image ${IMAGE_NAME} is not built yet. Building..."
@@ -24,13 +22,13 @@ if [ -z "${is_image_created}" ]; then
         jagger-webclient.jar
 
     docker build -t ${IMAGE_NAME}:${VERSION_TAG} .
-    [[ $? -eq 0  ]] || { echo "Failed to build the image." >&2; exit 1; }
+    [[ $? -eq 0  ]] || { echo "Failed to build ${IMAGE_NAME} image." >&2; exit 1; }
 
     rm -fv jagger-webclient.jar
 fi
 
 # Step 2. Check and create the container if needed.
-echo "'Check and create the container if needed' phase..."
+echo "'Check and create the container if needed' phase for ${CONTAINER_NAME} container..."
 is_container_created=$(docker ps -aq -f name=${CONTAINER_NAME})
 if [ -z "${is_container_created}" ]; then
     echo "Container ${CONTAINER_NAME} is not created. Creating..."
@@ -44,14 +42,14 @@ if [ -z "${is_container_created}" ]; then
       --env JWC_JDBC_PASS=${JWC_JDBC_PASS} \
       --name ${CONTAINER_NAME} \
       ${IMAGE_NAME}:${VERSION_TAG}
-    [[ $? -eq 0  ]] || { echo "Failed to create the container." >&2; exit 1; }
+    [[ $? -eq 0  ]] || { echo "Failed to create ${CONTAINER_NAME} container" >&2; exit 1; }
 fi
 
 # Step 3. Check and start the container if needed.
-echo "'Check and start the container if needed' phase..."
+echo "'Check and start the container if needed' phase for ${CONTAINER_NAME} container..."
 is_container_running=$(docker ps -q -f name=${CONTAINER_NAME})
 if [ -z "${is_container_running}" ]; then
     echo "Container ${CONTAINER_NAME} is not started yet. Starting..."
     docker start ${CONTAINER_NAME}
-    [[ $? -eq 0  ]] || { echo "Failed to start the container." >&2; exit 1; }
+    [[ $? -eq 0  ]] || { echo "Failed to start ${CONTAINER_NAME} container." >&2; exit 1; }
 fi
