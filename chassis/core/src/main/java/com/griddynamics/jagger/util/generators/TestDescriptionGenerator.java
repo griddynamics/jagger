0165861b673359dff58f9af2405c687fc1446f0e
asokol <asokol@griddynamics.com>
2016-11-08 21:32:29 +0300

    validator was added

--- chassis/core/src/main/java/com/griddynamics/jagger/util/generators/TestDescriptionGenerator.java
+++ chassis/core/src/main/java/com/griddynamics/jagger/util/generators/TestDescriptionGenerator.java
@@ -1,24 +1,27 @@
 package com.griddynamics.jagger.util.generators;
 
 import com.griddynamics.jagger.engine.e1.collector.CollectThreadsTestListener;
+import com.griddynamics.jagger.engine.e1.collector.DiagnosticCollectorProvider;
+import com.griddynamics.jagger.engine.e1.collector.DurationCollector;
+import com.griddynamics.jagger.engine.e1.collector.InformationCollector;
 import com.griddynamics.jagger.engine.e1.collector.MetricDescription;
+import com.griddynamics.jagger.engine.e1.collector.NotNullResponseValidator;
+import com.griddynamics.jagger.engine.e1.collector.SimpleMetricCalculator;
+import com.griddynamics.jagger.engine.e1.collector.StdDevMetricAggregatorProvider;
 import com.griddynamics.jagger.engine.e1.collector.SuccessRateAggregatorProvider;
 import com.griddynamics.jagger.engine.e1.collector.SuccessRateCollectorProvider;
 import com.griddynamics.jagger.engine.e1.collector.SuccessRateFailsAggregatorProvider;
-import com.griddynamics.jagger.engine.e1.scenario.KernelSideObjectProvider;
+import com.griddynamics.jagger.engine.e1.collector.SumMetricAggregatorProvider;
+import com.griddynamics.jagger.engine.e1.collector.ValidationCollectorProvider;
 import com.griddynamics.jagger.engine.e1.scenario.OneNodeCalibrator;
-import com.griddynamics.jagger.engine.e1.scenario.ScenarioCollector;
+import com.griddynamics.jagger.engine.e1.scenario.ReflectionProvider;
 import com.griddynamics.jagger.engine.e1.scenario.WorkloadTask;
 import com.griddynamics.jagger.invoker.QueryPoolScenarioFactory;
 import com.griddynamics.jagger.invoker.RoundRobinPairSupplierFactory;
 import com.griddynamics.jagger.invoker.SimpleCircularLoadBalancer;
 import com.griddynamics.jagger.user.test.configurations.JTestDescription;
 import com.griddynamics.jagger.util.StandardMetricsNamesUtil;
-import org.springframework.beans.factory.config.RuntimeBeanReference;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.springframework.beans.factory.support.ManagedList;
 
 import static com.google.common.collect.Lists.newArrayList;
 
@@ -29,7 +32,7 @@ import static com.google.common.collect.Lists.newArrayList;
  */
 class TestDescriptionGenerator {
 
-    static WorkloadTask generatePrototype(JTestDescription jTestDescription) {
+    public static WorkloadTask generatePrototype(JTestDescription jTestDescription) {
         WorkloadTask prototype = new WorkloadTask();
         prototype.setCalibrator(new OneNodeCalibrator());
         prototype.setDescription(jTestDescription.getDescription());
@@ -45,16 +48,15 @@ class TestDescriptionGenerator {
         prototype.setScenarioFactory(scenarioFactory);
         prototype.setName(jTestDescription.getId());
 
+        ManagedList collectors = new ManagedList();
+        collectors.add(getSuccessRateMetric());
+        collectors.add(ReflectionProvider.ofClass(DurationCollector.class));
+        collectors.add(ReflectionProvider.ofClass(InformationCollector.class));
+        ValidationCollectorProvider validationCollectorProvider = new ValidationCollectorProvider();
+        validationCollectorProvider.setValidator(ReflectionProvider.ofClass(NotNullResponseValidator.class));
+        collectors.add(validationCollectorProvider);
 
-
-
-//
-//        List<KernelSideObjectProvider<ScenarioCollector<Object, Object, Object>>> allMetrics = new ArrayList<>();
-//        allMetrics.add((KernelSideObjectProvider<ScenarioCollector<Object, Object, Object>>) new RuntimeBeanReference("durationCollector"));
-//        allMetrics.add((KernelSideObjectProvider<ScenarioCollector<Object, Object, Object>>) new RuntimeBeanReference("informationCollector"));
-//        allMetrics.add(getSuccessRateMetric());
-//        prototype.setCollectors(allMetrics);
-        prototype.setCollectors(Collections.singletonList(getSuccessRateMetric()));
+        prototype.setCollectors(collectors);
         prototype.setTestListeners(newArrayList(new CollectThreadsTestListener()));
 
         return prototype;
@@ -72,4 +74,5 @@ class TestDescriptionGenerator {
         successRateCollectorProvider.setName("SR");
         return successRateCollectorProvider;
     }
+
 }
