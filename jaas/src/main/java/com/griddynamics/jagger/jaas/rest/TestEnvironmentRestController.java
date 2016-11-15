0c29ad47a6903cb1e299f228d8296756374a2157
anton-antonenko <kingtoxa@gmail.com>
2016-11-15 15:45:00 +0200

    JFG-959: add Next-config-to-execute header on PUT request to /envs

--- jaas/src/main/java/com/griddynamics/jagger/jaas/rest/TestEnvironmentRestController.java
+++ jaas/src/main/java/com/griddynamics/jagger/jaas/rest/TestEnvironmentRestController.java
@@ -1,17 +1,17 @@
 package com.griddynamics.jagger.jaas.rest;
 
-import static org.springframework.http.HttpStatus.OK;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
-
 import com.griddynamics.jagger.jaas.exceptions.ResourceAlreadyExistsException;
 import com.griddynamics.jagger.jaas.exceptions.ResourceNotFoundException;
 import com.griddynamics.jagger.jaas.exceptions.TestEnvironmentInvalidIdException;
 import com.griddynamics.jagger.jaas.exceptions.TestEnvironmentSessionNotFoundException;
 import com.griddynamics.jagger.jaas.exceptions.WrongTestEnvironmentStatusException;
+import com.griddynamics.jagger.jaas.service.JobExecutionService;
 import com.griddynamics.jagger.jaas.service.TestEnvironmentService;
+import com.griddynamics.jagger.jaas.storage.model.JobEntity;
+import com.griddynamics.jagger.jaas.storage.model.JobExecutionEntity;
 import com.griddynamics.jagger.jaas.storage.model.TestEnvUtils;
 import com.griddynamics.jagger.jaas.storage.model.TestEnvironmentEntity;
+import com.griddynamics.jagger.jaas.storage.model.TestSuiteEntity;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -29,13 +29,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 @RestController
 @RequestMapping(value = "/envs")
@@ -50,9 +54,12 @@ public class TestEnvironmentRestController extends AbstractController {
 
     private TestEnvironmentService testEnvService;
 
+    private JobExecutionService jobExecutionService;
+
     @Autowired
-    public TestEnvironmentRestController(TestEnvironmentService testEnvironmentService) {
+    public TestEnvironmentRestController(TestEnvironmentService testEnvironmentService, JobExecutionService jobExecutionService) {
         this.testEnvService = testEnvironmentService;
+        this.jobExecutionService = jobExecutionService;
     }
 
     @GetMapping(value = "/{envId}", produces = APPLICATION_JSON_VALUE)
@@ -97,7 +104,23 @@ public class TestEnvironmentRestController extends AbstractController {
         testEnv.setEnvironmentId(envId);
         TestEnvironmentEntity updated = testEnvService.update(testEnv);
         response.addCookie(getSessionCookie(updated));
+        String testSuiteNameToExecute = getTestSuiteNameToExecute(updated);
+        if (testSuiteNameToExecute != null) {
+            setNextConfigToExecuteHeader(response, testSuiteNameToExecute);
+        }
         return ResponseEntity.accepted().build();
+    }
+
+    private String getTestSuiteNameToExecute(TestEnvironmentEntity testEnv) {
+        List<String> testSuitesNames = testEnv.getTestSuites().stream().map(TestSuiteEntity::getTestSuiteId).collect(toList());
+
+        return jobExecutionService.readAll().stream()
+                .map(JobExecutionEntity::getJob)
+                .filter(job -> job.getEnvId().equals(testEnv.getEnvironmentId()))
+                .map(JobEntity::getTestSuiteId)
+                .filter(testSuitesNames::contains)
+                .findFirst()
+                .orElse(null);
     }
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
@@ -134,6 +157,10 @@ public class TestEnvironmentRestController extends AbstractController {
 
     private void setExpiresHeader(HttpServletResponse response, TestEnvironmentEntity testEnv) {
         response.addHeader(TestEnvUtils.EXPIRES_HEADER, getFormattedExpirationDate(testEnv));
+    }
+
+    private void setNextConfigToExecuteHeader(HttpServletResponse response, String testSuiteName) {
+        response.addHeader(TestEnvUtils.CONFIG_NAME_HEADER, testSuiteName);
     }
 
     private Cookie getSessionCookie(TestEnvironmentEntity testEnv) {
