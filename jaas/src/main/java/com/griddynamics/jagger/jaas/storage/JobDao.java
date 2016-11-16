06fae280b0bdbc7327b22ad8f98074907dde97af
anton-antonenko <kingtoxa@gmail.com>
2016-11-16 19:13:11 +0200

    JFG-957: add JobExecutionController;  remove unused methods in JobExecutionService;

--- jaas/src/main/java/com/griddynamics/jagger/jaas/storage/JobDao.java
+++ jaas/src/main/java/com/griddynamics/jagger/jaas/storage/JobDao.java
@@ -1,11 +1,9 @@
 package com.griddynamics.jagger.jaas.storage;
 
 import com.griddynamics.jagger.jaas.storage.model.JobEntity;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface JobDao extends CrudDao<JobEntity, Long> {
-    @Transactional
     List<JobEntity> readByEnvAndTestSuite(String envId, String testSuiteId);
 }
