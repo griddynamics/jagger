6f53983cd48980a7ab95c3ac1a0420f9bb2db31f
anton-antonenko <kingtoxa@gmail.com>
2016-12-13 12:44:35 +0200

    revert JFG-1037: add test.properties separated from environment.properties to java builders archetype

--- archetype-java-builders/src/main/resources/archetype-resources/src/main/java/ExampleJLoadScenarioProvider.java
+++ archetype-java-builders/src/main/resources/archetype-resources/src/main/java/ExampleJLoadScenarioProvider.java
@@ -30,6 +30,7 @@ import com.griddynamics.jagger.util.JaggerPropertiesProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
 /**
