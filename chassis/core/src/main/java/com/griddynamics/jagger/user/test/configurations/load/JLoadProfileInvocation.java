21024ed601115da428f66c9bd3750cd512a33a3e
asokol <asokol@griddynamics.com>
2016-12-14 16:41:01 +0300

    JLoadInvocation

---
+++ chassis/core/src/main/java/com/griddynamics/jagger/user/test/configurations/load/JLoadProfileInvocation.java
@@ -0,0 +1,8 @@
+package com.griddynamics.jagger.user.test.configurations.load;
+
+/**
+ * @author asokol
+ *         created 12/14/16
+ */
+public class JLoadProfileInvocation {
+}
---
+++ chassis/core/src/main/java/com/griddynamics/jagger/user/test/configurations/load/auxiliary/InvocationCount.java
@@ -0,0 +1,8 @@
+package com.griddynamics.jagger.user.test.configurations.load.auxiliary;
+
+/**
+ * @author asokol
+ *         created 12/14/16
+ */
+public class InvocationCount {
+}
