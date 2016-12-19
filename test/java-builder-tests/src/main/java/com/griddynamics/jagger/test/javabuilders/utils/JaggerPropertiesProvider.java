faccf4bc8bfe1e49e932586adb685180d02498b7
oskliarov <oskliarov@griddynamic.com>
2016-12-19 14:20:57 +0200

    [JFG-944] java builders smoke tests

---
+++ test/java-builder-tests/src/main/java/com/griddynamics/jagger/test/javabuilders/utils/JaggerPropertiesProvider.java
@@ -0,0 +1,20 @@
+package com.griddynamics.jagger.test.javabuilders.utils;
+
+import com.griddynamics.jagger.util.JaggerXmlApplicationContext;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class JaggerPropertiesProvider implements ApplicationContextAware {
+
+    private ApplicationContext context;
+
+    public String getPropertyValue(String key) {
+        return ((JaggerXmlApplicationContext) context).getEnvironmentProperties().getProperty(key);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+}
