ae7cc3a20e5c8750c2a5dfddf1efb37d0b04295c
aantonenko <kingtoxa@gmail.com>
2017-01-10 12:29:23 +0200

    JFG-962: fixes after review

--- chassis/core/src/main/java/com/griddynamics/jagger/user/test/configurations/JTestDefinition.java
+++ chassis/core/src/main/java/com/griddynamics/jagger/user/test/configurations/JTestDefinition.java
@@ -7,6 +7,8 @@ import com.griddynamics.jagger.engine.e1.collector.invocation.InvocationListener
 import com.griddynamics.jagger.invoker.Invoker;
 import com.griddynamics.jagger.invoker.QueryPoolLoadBalancer;
 import com.griddynamics.jagger.invoker.RoundRobinLoadBalancer;
+import com.griddynamics.jagger.invoker.RoundRobinPairSupplierFactory;
+import com.griddynamics.jagger.invoker.SimpleCircularLoadBalancer;
 import com.griddynamics.jagger.invoker.v2.DefaultHttpInvoker;
 import com.griddynamics.jagger.user.test.configurations.auxiliary.Id;
 
@@ -80,6 +82,9 @@ public class JTestDefinition {
         private Builder(Id id, Iterable endpointsProvider) {
             this.id = id;
             this.endpointsProvider = endpointsProvider;
+            this.loadBalancer = new SimpleCircularLoadBalancer() {{
+                setPairSupplierFactory(new RoundRobinPairSupplierFactory());
+            }};
         }
 
         /**
