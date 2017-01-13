075d4febb1f2bf19cd36084a9d4083d5a848f17b
aantonenko <kingtoxa@gmail.com>
2017-01-13 13:24:16 +0200

    JFG-1103: make LoadBalancerProvider Serializable

--- chassis/core/src/main/java/com/griddynamics/jagger/user/test/configurations/auxiliary/LoadBalancerProvider.java
+++ chassis/core/src/main/java/com/griddynamics/jagger/user/test/configurations/auxiliary/LoadBalancerProvider.java
@@ -2,10 +2,12 @@ package com.griddynamics.jagger.user.test.configurations.auxiliary;
 
 import com.griddynamics.jagger.invoker.QueryPoolLoadBalancer;
 
+import java.io.Serializable;
+
 /**
  * Provides load balancer aka distributor (how to pair endpoints and queries) (subtypes of {@link QueryPoolLoadBalancer}).
  */
-public class LoadBalancerProvider {
+public class LoadBalancerProvider implements Serializable {
     private final QueryPoolLoadBalancer loadBalancer;
 
     public LoadBalancerProvider(QueryPoolLoadBalancer loadBalancer) {this.loadBalancer = loadBalancer;}
